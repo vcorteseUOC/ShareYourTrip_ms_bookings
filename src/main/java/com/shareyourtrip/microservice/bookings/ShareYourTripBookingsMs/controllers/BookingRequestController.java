c78d65fcfac4e0867203dd6fab4aa00509809482
DESKTOP-OS8GTUJ\SWB 94 <vcortese@uoc.edu>
2026-04-23 13:18:14 +0200

    Security added

--- src/main/java/com/shareyourtrip/microservice/bookings/ShareYourTripBookingsMs/controllers/BookingRequestController.java
+++ src/main/java/com/shareyourtrip/microservice/bookings/ShareYourTripBookingsMs/controllers/BookingRequestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/booking-requests")
+@RequestMapping("/booking-requests")
 @RequiredArgsConstructor
 public class BookingRequestController {
 
